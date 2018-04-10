6c17a45e41281c6954ffe5a9c66d65c077f91411
SimePel <simepelmain@gmail.com>
2018-04-10 11:54:40 +0700

    Add fetching data from form

---
+++ handlers/handlers.go
@@ -0,0 +1,31 @@
+package handlers
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+var t = template.Must(template.New("pets-health").ParseGlob("templates/*.html"))
+
+// FormView shows form to fill the db
+func FormView(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	t.ExecuteTemplate(w, "db", nil)
+}
+
+// FillDB @TODO
+func FillDB(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	if err := r.ParseForm(); err != nil {
+		log.Println("Form parsing: ", err)
+		http.Error(w, "Problems with fetching your data from a form. Please try again", http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Printf("Name: %s\nSymptoms: %s\n,Therapy: %s\nPets: %s\n",
+		r.FormValue("disease"), r.FormValue("symptoms"), r.FormValue("therapy"), r.FormValue("pets"))
+
+	http.Redirect(w, r, "/fill-db", http.StatusSeeOther)
+}
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/Adictes/pets-health/handlers"
+	"github.com/julienschmidt/httprouter"
+)
+
+func main() {
+	router := httprouter.New()
+
+	router.ServeFiles("/assets/*filepath", http.Dir("assets/"))
+
+	router.GET("/fill-db", handlers.FormView)
+	router.POST("/fill-db", handlers.FillDB)
+
+	err := http.ListenAndServe(":8080", router)
+	if err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
