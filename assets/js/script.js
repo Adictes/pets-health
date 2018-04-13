57cb3286c206ec29e4765397172935ddfd63f66e
Evgenia <jane.mityukova@gmail.com>
2018-04-14 01:34:26 +0700

    add petnumber translating

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,22 @@ window.onload = function () {
 
 
 	function showContent() {
+
+		var z = document.getElementsByName('a');
+		for (var i = 0; i < z.length; i++)  {
+
+    if  (z[i].checked) {
+
+     var  petNumber = i; break;
+
+    }
+
+}
+
+alert(s);
+
+
+
 		var output = '';
 
 		output += '<div class="naming"> <div class="percentage">Проценты</div><div class="recipe">Диагноз и рекомендации</div></div>'
@@ -13,10 +29,47 @@ window.onload = function () {
 		document.getElementById('content').innerHTML = output;
 
 		var query = document.getElementById("theSearch").innerText;
-		var req = '{"name": "todo", "query":"' + query + '"}';
+		var req = '{"name": "'+petNumber'", "query":"' + query + '"}';
 		wsr.send(JSON.parse(req))
+
+
 	}
 
 	document.getElementById('searchButton').onclick = showContent;
 
+
+
+
+
+
+
 }
+
+
+	/*
+wsr.onmessage = function(event){
+
+	var data=JSON.parse(event.data); //4 поля 
+	/*data.name - имя болезни
+	pets -
+	symp- 
+	therapy - лечение
+
+}
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
