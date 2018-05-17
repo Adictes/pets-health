4950ccb2bf697667268e36d0d54500cb718d5a58
SimePel <simepelmain@gmail.com>
2018-05-17 21:48:37 +0700

    Refactoring js and css with prettier

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,54 +2,40 @@ var wsr = new WebSocket("ws://" + window.location.host + "/wsr");
 
 var dict = { 0: "собака", 1: "кошка" };
 
-window.onload = function () {
+window.onload = function() {
+    function showContent() {
+        var z = document.getElementsByName("a");
+        for (var i = 0; i < z.length; i++) {
+            if (z[i].checked) {
+                var pet = dict[i];
+                break;
+            }
+        }
 
+        var output = "";
 
-	function showContent() {
+        output +=
+            '<div class="naming"> <div class="percentage">Вероятность болезни</div><div class="recipe">Диагноз и рекомендации</div></div>';
 
-		var z = document.getElementsByName('a');
-		for (var i = 0; i < z.length; i++) {
+        document.getElementById("content").innerHTML = output;
 
-			if (z[i].checked) {
+        var query = document.getElementById("theSearch").value;
+        var req = '{ "name":"' + pet + '", "query": "' + query + '" }';
+        wsr.send(req);
+    }
 
-				var pet = dict[i]; break;
+    document.getElementById("searchButton").onclick = showContent;
+};
 
-			}
+wsr.onmessage = function(event) {
+    var data = JSON.parse(event.data);
 
-		}
+    var output =
+        '<div class="naming"><div class="percentage">%</div><div class="recipe">' +
+        data.name +
+        ":  " +
+        data.therapy +
+        "</div></div>";
 
-
-		var output = '';
-
-		output += '<div class="naming"> <div class="percentage">Проценты</div><div class="recipe">Диагноз и рекомендации</div></div>';
-
-		document.getElementById('content').innerHTML = output;
-
-		var query = document.getElementById("theSearch").value;
-		var req = '{ "name":"' + pet + '", "query": "' + query + '" }';
-		wsr.send(req);
-
-	}
-
-	document.getElementById('searchButton').onclick = showContent;
-
-
-
-
-
-
-
-}
-
-
-
-wsr.onmessage = function (event) {
-
-	//var data=JSON.parse(event.data); 
-	var dataName = 'имя болезни';
-	var dataTherapy = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
-
-	var output = '<br><div class="naming"> <div class="percentage">Проценты</div><div class="recipe">' + dataName + '  ' + dataTherapy + '</div></div>';
-
-	document.getElementById('content').innerHTML += output;
-}
+    document.getElementById("content").innerHTML += output;
+};
